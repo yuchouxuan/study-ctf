0f6964d13cf8eb20a8f019f9289048fad2111051
yuchouxuan <youchouxuan@live.com>
2024-03-29 15:20:53 +0800

    o

--- utfc/fstr.py
+++ utfc/fstr.py
@@ -28,6 +28,16 @@ def strcolor(s, color=cmd_color.none):
     return color + s + cmd_color.none
 
 class fstr(str):
+
+    @staticmethod 
+    def randRegX(rt = 'B+v*'):
+        '''生成随机符合正则的字符串'''
+        ret = rstr.xeger(rt)
+        print('- '*30)
+        print('RandRSt:', ret)
+        print('- ' * 30)
+        return ret
+    
     def BinMath(op1, op2 = b'\x50\x4b\x03\x04', op='^'):
         '''位运算'''
         if isinstance(op2,str):
@@ -55,7 +65,10 @@ class fstr(str):
         return self.BinMath(right)
 
     def __and__(self,right):
-        return self.BinMath(right,'+')
+        return self.BinMath(right,'&')
+
+    def __or__(self,right):
+        return self.BinMath(right,'|')
 
     def base(self,encoder=None):
         '''打base
@@ -76,21 +89,22 @@ class fstr(str):
         except:
             return bout
 
-        
-    
-    def tourl(self,cs='utf-8'):
+    def url(self,force=False,cs='utf-8'):
+        '''url编码,force=True时,强制对所有字符编码'''
         ret =''
-        for i in self.encode(cs):
-            ret+='%%%x'%i
+        if force:
+             for i in self.encode(cs):
+                ret+='%%%x'%i
+        else:
+            ret =  parse.quote(self,encoding=cs)           
         return fstr(ret.upper())
+
     @staticmethod
     def fromurl(url):
+        '''url解码'''
         return fstr(parse.unquote(url))
+
     @staticmethod
-    
-
-
-
     def __new__(cls,strin,o: object = ...):
         if isinstance(strin,bytes):
             try:
@@ -98,7 +112,6 @@ class fstr(str):
             except:
                 printc('[*]Byte->Str,decode Error',cmd_color.red)
         return super().__new__(cls,strin)
-
         
     def re(self,strfind,only1=True,flag=re.IGNORECASE+re.M):
         """正则-findall
@@ -115,7 +128,52 @@ class fstr(str):
         """转16进制"""
         return ''.join(['{:02x}'.format(ord(c)) for c in self])
 
-    def fChar(self):
+    def hex_c(strin, mask='\\x'):
+        """转16进制_c格式"""
+        return fstr(''.join([mask+'{:02x}'.format(ord(c)) for c in strin]))
+    
+    def charRange(st='a',end='z'):
+        if isinstance(st, str): st = ord(st)
+        if isinstance(end, str): end = ord(end)
+        return fstr('',join([chr(i) for i in range(st, end + 1)]))
+    
+    def haveAny(self,sfind) -> bool:
+        '''包含任意'''
+        if isinstance(sfind,str):
+            return sfind in self 
+        elif isinstance(sfind,bytes):
+            return sfind in self.encode() 
+        elif isinstance(sfind,(set,list,tuple)) :
+            for i in sfind:
+                if self.haveAny(i) :
+                    return True 
+            return False 
+        elif isinstance(sfind,dict):
+            for v in sfind.values():
+                if self.haveAny(v) :
+                    return True 
+            return False 
+        return False 
+
+    def haveAll(self,sfind)->bool:
+        '''包含所有'''
+        if isinstance(sfind,str):
+            return sfind in self 
+        elif isinstance(sfind,bytes):
+            return sfind in self.encode() 
+        elif isinstance(sfind,(set,list,tuple)) :
+            for i in sfind:
+                if not self.haveAll(i) :
+                    return False 
+            return True 
+        elif isinstance(sfind,dict):
+            for v in sfind.values():
+                if not self.haveAll(v) :
+                    return False 
+            return True 
+        return False 
+
+    def fullChar(self):
         """半角字符转全角"""
         full = ''
         for ch in self:
@@ -128,7 +186,7 @@ class fstr(str):
             full += ch
         return full 
     
-    def reqhc(self,splitby=['\n',':'],delspace=True)->dict:
+    def toHeader(self,splitby=['\n',':'],delspace=True)->dict:
         """字符串转字典,用于构造requests请求头和cookie,
         delspace用于清理没用的头尾空格"""
         ret ={}
@@ -146,7 +204,7 @@ class fstr(str):
                 else :
                     ret[i]=''
         return ret
-    
+ 
     @staticmethod
     def read(fn,cs='utf-8') :
         """直接从文件中读取字符串"""
@@ -158,17 +216,24 @@ class fstr(str):
                 printc('[-]ERR:can decode by'+cs,cmd_color.red)
                 pass 
         return fstr(s)
-        
+    
+    def write(self,fn,openFlag='w'):
+        with open(fn,openFlag) as f :
+            return f.write(self)
+
     def __getitem__(self,i):
         if isinstance(i,int):
             i%=len(self)
         return super().__getitem__(i)
         
-       
+    def replaceloop(self,str:s,str:d):  
+        ret = self
+        where ret. 
  
 if __name__ == "__main__":
-    a = fstr('flag')
-    print(a.frombase('ZmxhZw=='))
+    a = fstr('flag啊')
+    print(a.haveAll(['a','f']))
+
 
  
 
